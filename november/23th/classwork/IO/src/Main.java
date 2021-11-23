3a5b6da7f5f427f61c95cf820406ced48711f28f
Sergey Anshin <anshin.serega@gmail.com>
2021-11-24 01:54:43 +0300

    added classwork "IO"

---
+++ november/23th/classwork/IO/src/Main.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        try {
+//            FileInputStream fileInputStream = new FileInputStream("testFile.txt");
+//            int c;
+//            while ((c = fileInputStream.read()) != -1) {
+//                System.out.print((char) c);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            FileOutputStream fileOutputStream = new FileOutputStream("testFileTwo.txt");
+//            String variableForWrite = "TeachMeSkills";
+//            fileOutputStream.write(variableForWrite.getBytes(StandardCharsets.UTF_8));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            FileReader fileReader = new FileReader("testFileTwo.txt");
+//            char[] chars = new char[13];
+//            fileReader.read(chars);
+//            System.out.println(Arrays.toString(chars));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        try (FileWriter fileWriter  = new FileWriter("testFile.txt")) {
+//            String variableForWrite = "Teach me Skills";
+//            fileWriter.write(variableForWrite);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("testFile.txt"))) {
+//            String variableForWrite = "teach me skills";
+//            bufferedWriter.write(variableForWrite);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("testFile.txt"))) {
+//            System.out.println(bufferedReader.readLine());
+//            String line;
+//            while ((line = bufferedReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+}
