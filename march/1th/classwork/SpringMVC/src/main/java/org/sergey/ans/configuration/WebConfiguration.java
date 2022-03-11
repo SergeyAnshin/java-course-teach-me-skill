41407d63c31c43094f08e28aab768a533f82117c
SergeyAnshin <anshin.serega@gmail.com>
2022-03-12 02:31:51 +0300

    added classwork "Interceptor and Thymeleaf"

--- march/1th/classwork/SpringMVC/src/main/java/org/sergey/ans/configuration/WebConfiguration.java
+++ march/1th/classwork/SpringMVC/src/main/java/org/sergey/ans/configuration/WebConfiguration.java
@@ -1,23 +1,88 @@
 package org.sergey.ans.configuration;
 
+import org.sergey.ans.interceptor.TestInterceptor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan("org.sergey.ans")
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfiguration extends WebMvcConfigurerAdapter implements WebMvcConfigurer, ApplicationContextAware {
+    private TestInterceptor testInterceptor;
+    private TestInterceptor testInterceptorTwo;
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    public WebConfiguration(TestInterceptor testInterceptor, TestInterceptor testInterceptorTwo) {
+        this.testInterceptor = testInterceptor;
+    }
+
+//    @Bean
+//    public ViewResolver internalResourceViewResolver() {
+//        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+//        bean.setPrefix("/pages/");
+//        bean.setSuffix(".jsp");
+//        return bean;
+//    }
+
+    /**
+     * (*) - отрабатывает 1 любая секция контроллера
+     * (**) - отрабатывают все секции контроллера (user/reg/something - секция)
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(testInterceptor)
+                .addPathPatterns("/user/reg")
+                .order(2);
+//                .addPathPatterns("/user/**");
+//                .excludePathPatterns("/user/reg");
+        registry.addInterceptor(testInterceptorTwo)
+                .addPathPatterns("/user/reg")
+                .order(1);
+    }
 
     @Bean
-    public ViewResolver internalResourceViewResolver() {
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setPrefix("/pages/");
-        bean.setSuffix(".jsp");
-        return bean;
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("/pages/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
